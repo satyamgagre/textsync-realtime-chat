65d93c9004cf60a219b1f79421e1e2c7d519fe03
Satyam Gagare <satyamgagre4@gmail.com>
2026-01-19 02:48:47 +0530

    initialize auth controller and implement signup logic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,17 +1,54 @@
-export const signup = async (req, res) => {
-    try {
-        const {fullName, userName, password, confirmPassword, gender} = req.body;
-    } catch (error) {
-        
-    }
-}
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
-export const login= (req, res) => {
-    res.send("Login User")
-    console.log("loginUser");
-}
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Passwords do not match" });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const maleAvatar = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
+    const femaleAvatar = `https://api.dicebear.com/7.x/personas/svg?seed=${username}`;
+
+    const newUser = new User({
+    fullName,
+    username,
+    password: hashedPassword,
+    gender,
+    profilePic: gender === "Male" ? maleAvatar : femaleAvatar,
+    });
+
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    console.error("Error in Signup Controller:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const login = (req, res) => {
+  console.log("loginUser");
+  res.send("Login User");
+};
 
 export const logout = (req, res) => {
-    res.send("Logout User")
-    console.log("logoutUser");
-}
+  console.log("logoutUser");
+  res.send("Logout User");
+};
--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,19 @@ import authRoutes from "./routes/auth.routes.js";
 import conncetToMongoDb from "./db/connectToMongoDb.js";
 
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    // root route http://localhost:5000/
-    res.send("Hello World")
-});
+dotenv.config();
+
+app.use(express.json());
 
 app.use("/api/auth/",authRoutes)
+
+// app.get("/", (req, res) => {
+ // root route http://localhost:5000/
+//     res.send("Hello World")
+// });
+
 
 app.listen(PORT, () => { 
     conncetToMongoDb();
