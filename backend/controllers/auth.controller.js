d89d376f6c9091a56813dd64ee85c7e574a1eabf
Satyam Gagare <satyamgagre4@gmail.com>
2026-01-20 01:13:01 +0530

    fetch database conncetion and JWT authentication  with secure cookies

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -28,7 +29,9 @@ export const signup = async (req, res) => {
     profilePic: gender === "Male" ? maleAvatar : femaleAvatar,
     });
 
-
+if (newUser) {
+  // generate JWT token here
+    generateTokenAndSetCookie(newUser._id, res);
     await newUser.save();
 
     res.status(201).json({
@@ -37,6 +40,9 @@ export const signup = async (req, res) => {
       username: newUser.username,
       profilePic: newUser.profilePic,
     });
+  } else {
+    res.status(400).json({error:"Invalid User data"});
+  }
   } catch (error) {
     console.error("Error in Signup Controller:", error);
     res.status(500).json({ error: "Internal Server Error" });
