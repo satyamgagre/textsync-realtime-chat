d3a2915d0134cc4509be62108a8b3bd60236cba7
Satyam Gagare <satyamgagre4@gmail.com>
2026-01-31 00:14:45 +0530

    get conversations from database

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,6 +1,8 @@
 import Conversation from './Conversation.jsx';
+import useGetConversations from '../../hooks/useGetConversations.js';
 
 const Conversations = () => { 
+    useGetConversations();
     return (
         <div className="py-2 flex flex-col overflow-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-slate-700/50 hover:scrollbar-thumb-slate-600/50">
             <Conversation />
