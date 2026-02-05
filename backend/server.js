e8f857ae1edb83ef23982da9f6213d82e2d49ab9
Satyam Gagare <satyamgagre4@gmail.com>
2026-02-06 01:58:58 +0530

    //////////////////////////////////////

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,10 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import conncetToMongoDb from "./db/connectToMongoDb.js";
-import {app, server} from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -19,9 +18,9 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.use("/api/users", userRoutes); 
+app.use("/api/users", userRoutes);
 
 server.listen(PORT, () => {
-    conncetToMongoDb();
-    console.log(`Server is running on port: ${PORT}`);
+conncetToMongoDb();
+console.log(`Server is running on port: ${PORT}`);
 });
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
-import { AuthContextProvider } from './context/AuthContext.jsx';
-import { SocketContext, SocketContextProvider } from './context/socketContext.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { SocketContextProvider } from "./context/socketContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AuthContextProvider>
-    <BrowserRouter>
-      <SocketContextProvider>
-        <App />
-      </SocketContextProvider>
-    </BrowserRouter>
-    </AuthContextProvider>
-  </StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<AuthContextProvider>
+				<SocketContextProvider>
+					<App />
+				</SocketContextProvider>
+			</AuthContextProvider>
+		</BrowserRouter>
+	</React.StrictMode>
+);
