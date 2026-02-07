e9fb0fd5ad51e8fa6fe24940e40d21a0a4ee4b19
Satyam Gagare <satyamgagre4@gmail.com>
2026-02-07 22:33:40 +0530

    Serve SPA index, add scripts, update .gitignore

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
 conncetToMongoDb();
